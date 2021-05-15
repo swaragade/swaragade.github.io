6c2ba46219390b900f1825a3f22dfc0b713d80f6
Suraj Waragade <suraj_waragade@live.com>
2021-05-15 14:44:10 +0530

    Stable:v1

--- src/portfolio.js
+++ src/portfolio.js
@@ -183,7 +183,7 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to true
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
   display: false // Set false to hide this section, defaults to true
 };
 
